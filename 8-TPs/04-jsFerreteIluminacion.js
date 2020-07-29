ae487e72d9f8bb6012255ac797513d0d6991fec8
JoaquinTrassens <trassensjoaquin@gmail.com>
2020-07-29 02:02:52 -0300

    ulima actualizacion

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,3 +1,4 @@
+// Trassens Joaquin
 /*4.	Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
@@ -12,6 +13,83 @@ del impuesto con el siguiente mensaje:
 ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
+function CalcularPrecio () 
+{
+ 	var cantidadLamparas;
+ 	var marcaLamparas;
+ 	var precioBruto;
+ 	
+ 	var descuentoCalculado;
+ 	var porcentaje;
+ 	var precioFinal;
+ 	var aumentoIIBB;
+
+
+ 	
+ 	cantidadLamparas=txtIdCantidad.value;
+ 	cantidadLamparas=parseInt(cantidadLamparas);
+
+ 	marcaLamparas=Marca.value;
+	precioBruto=cantidadLamparas*35;
+
+	switch(cantidadLamparas)
+	{
+		case 1:
+		case 2:
+			porcentaje=0;
+			break;
+		case 3:
+			switch(marcaLamparas)
+			{
+				case "ArgentinaLuz":
+					porcentaje=15;
+					break;
+				case "FelipeLamparas":
+					porcentaje=10;
+					break;
+				default:
+					porcentaje=5;
+					break;
+			}			
+			break;
+		case 4:
+			switch(marcaLamparas)
+			{
+				case "ArgentinaLuz":
+				case "FelipeLamparas":
+					porcentaje=25;
+					break;
+				default:
+					porcentaje=20;
+					break;
+			}
+			break;
+		case 5:
+			switch(marcaLamparas)
+			{
+				case "ArgentinaLuz":
+					porcentaje=40;
+					break;
+				default:
+					porcentaje=30;
+					break;
+			}
+ 			break;
+ 		default:
+ 			porcentaje=50;
+ 			break;
+	}
+	descuentoCalculado=precioBruto*porcentaje/100;
+	precioFinal=precioBruto-descuentoCalculado;
+	aumentoIIBB=0;
+	if(precioFinal>120)
+	{
+		aumentoIIBB=precioFinal*10/100;
+		alert("Usted pago "+aumentoIIBB+" de IIBB.")
+	}
+ 	txtIdprecioDescuento.value=precioFinal+aumentoIIBB;
+}
+/*
 function CalcularPrecio () 
 {
  	var cantidadDeLamparitas;
@@ -56,6 +134,5 @@ function CalcularPrecio ()
  		}
  	}*/
  	
- 	
- 	txtIdprecioDescuento.value=precioFinal-importeDescuento;
-}
+	
+
